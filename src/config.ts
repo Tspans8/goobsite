bd5806f202cd9bd01247eca1ab03ad43280341b4
Thomas Spanswick <78428155+Tspans8@users.noreply.github.com>
2025-05-05 14:20:37 -0400

    Update config.ts
    Update navbar with new wiki and fix docs link

--- src/config.ts
+++ src/config.ts
@@ -39,6 +39,11 @@ export const navBarConfig: NavBarConfig = {
     },
     {
       name: 'Wiki',
+      url: 'https://wiki.goobstation.com', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
+    },
+    {
+      name: 'Docs',
       url: 'https://docs.goobstation.com', // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
